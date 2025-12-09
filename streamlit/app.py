985506e095b5a811e5c3b5368674b723a195d2a6
Silfi <silfimaharani799@gmail.com>
2025-12-09 13:06:56 +0700

    Add initial implementation of form data collection using Streamlit

---
+++ streamlit/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title("Form Data Diri")
+st.write("Silahkan isi data diri anda")
+st.write("Made by Silfi")
+
+with st.form("form_data_diri"):
+    nama = st.text_input("Nama")
+    alamat = st.text_input("Alamat")
+    usia = st.number_input("Usia")
+    email = st.text_input("Email")
+    submit = st.form_submit_button("Submit")
+
+if submit:
+    st.success(f"Terima kasih {nama} telah mengisi form data diri")
+    st.write(f"Nama : {nama}")
+    st.write(f"Alamat : {alamat}")
+    st.write(f"Usia : {usia}")
+    st.write(f"Email : {email}")
+
+    
